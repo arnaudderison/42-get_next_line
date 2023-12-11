f18909a96aa65032de6f224969ca52115f5ca39e
arnaudderison <arnaud.derison@hotmail.com>
2023-12-11 14:05:01 +0100

    add test

--- main.c
+++ main.c
@@ -6,20 +6,27 @@
 /*   By: arnaud <arnaud@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/10 20:33:17 by arnaud            #+#    #+#             */
-/*   Updated: 2023/12/11 08:55:21 by arnaud           ###   ########.fr       */
+/*   Updated: 2023/12/11 14:02:30 by arnaud           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "get_next_line.h"
+#include "get_next_line_bonus.h"
 #include <fcntl.h>
 
 void	main(void)
 {
-	int fd;
+	int	fd_test;
+	int	fd_file;
 
-	fd = open("./test", O_RDONLY);
-
-	printf("%s", get_next_line(fd));
-	printf("%s", get_next_line(fd));
-	close(fd);
+	fd_test = open("./test", O_RDONLY);
+	fd_file = open("./file", O_RDONLY);
+	printf("%s", get_next_line(fd_test));
+	printf("%s", get_next_line(fd_file));
+	printf("%s", get_next_line(fd_test));
+	printf("%s", get_next_line(fd_file));
+	printf("%s", get_next_line(fd_test));
+	printf("%s", get_next_line(fd_file));
+	printf("%s", get_next_line(fd_test));
+	printf("%s", get_next_line(fd_file));
+	close(fd_test);
 }
