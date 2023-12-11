c0faa8fc183af16741d416107886d0fa44140526
arnaudderison <arnaud.derison@hotmail.com>
2023-12-11 09:06:26 +0100

    get line and cleaner

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: arnaud <arnaud@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/10 18:59:42 by arnaud            #+#    #+#             */
-/*   Updated: 2023/12/10 21:11:58 by arnaud           ###   ########.fr       */
+/*   Updated: 2023/12/11 09:04:39 by arnaud           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -46,7 +46,45 @@ char	*get_buffer_file(int fd, char *buffer)
 	}
 	return (buffer);
 }
+char	*get_line(char *buffer)
+{
+	char	*line;
+	int		len;
 
+	if (!buffer)
+		return (NULL);
+	len = 0;
+	while (buffer[len] && buffer[len] != '\n')
+		len++;
+	if (buffer[len] == '\n')
+		len++;
+	line = (char *)malloc(len + 1);
+	if (!line)
+		return (NULL);
+	ft_strlcpy(line, buffer, len + 1);
+	return (line);
+}
+char	*clean_buffer(char *line, char *buffer)
+{
+	int		start;
+	int		end;
+	int		size;
+	char	*new_buffer;
+
+	if (!line || !buffer)
+		return (NULL);
+	start = ft_strlen(line);
+	end = ft_strlen(buffer);
+	size = end - start;
+	if (size < 0)
+		return (NULL);
+	new_buffer = (char *)malloc(size + 1);
+	if (!new_buffer)
+		return (NULL);
+	ft_strlcpy(new_buffer, buffer + start, size + 1);
+	free(buffer);
+	return (new_buffer);
+}
 char	*get_next_line(int fd)
 {
 	static char *buffer;
@@ -55,6 +93,7 @@ char	*get_next_line(int fd)
 	if (fd < 0 || BUFFER_SIZE <= 0 || read(fd, 0, 0) < 0)
 		return (NULL);
 	buffer = get_buffer_file(fd, buffer);
-
-	printf("buffer: |%s|", buffer);
+	line = get_line(buffer);
+	buffer = clean_buffer(line, buffer);
+	return (line);
 }
